3b4d9150646d5cdcdfbfb219fc16479a1f786b79
OmkarKhilari <omkarkhilari95@gmail.com>
2024-07-15 23:22:32 +0530

    endpoint changed

--- blog-backend/main.go
+++ blog-backend/main.go
@@ -18,18 +18,18 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/posts", controller.GetPosts).Methods("GET")
-	router.HandleFunc("/posts/{id}", controller.GetPost).Methods("GET")
-	router.HandleFunc("/posts", controller.CreatePost).Methods("POST")
+	router.HandleFunc("blogshog/posts", controller.GetPosts).Methods("GET")
+	router.HandleFunc("blogshog/posts/{id}", controller.GetPost).Methods("GET")
+	router.HandleFunc("blogshog/posts", controller.CreatePost).Methods("POST")
 	// router.HandleFunc("/posts/{id}", controller.UpdatePost).Methods("PUT")
 	// router.HandleFunc("/posts/{id}", controller.DeletePost).Methods("DELETE")
 
-	router.HandleFunc("/users", controller.CreateUser).Methods("POST")
-	router.HandleFunc("/users", controller.GetUser).Methods("GET")
+	router.HandleFunc("blogshog/users", controller.CreateUser).Methods("POST")
+	router.HandleFunc("blogshog/users", controller.GetUser).Methods("GET")
 
 	headers := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
-	origins := handlers.AllowedOrigins([]string{"*", "https://blog-shog.vercel.app/"})
+	origins := handlers.AllowedOrigins([]string{"*", "https://blog-shog.vercel.app/", "https://blogshog.omkarkhilari.me/"})
 
 	log.Println("Server is running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(headers, methods, origins)(router)))
