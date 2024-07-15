505c9f968447178d7ca427ec664833900576e21d
OmkarKhilari <omkarkhilari95@gmail.com>
2024-07-15 17:05:43 +0530

    endpoint changed

--- blog-frontend/src/axiosInstance.js
+++ blog-frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://omkar.bhaskaraa45.me/',
+  baseURL: 'https://omkar.bhaskaraa45.me/blogshog',
 });
 
 export default instance;
