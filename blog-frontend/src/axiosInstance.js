6d08a9180bb1fd2aafe915cc2e818d55c5a04248
OmkarKhilari <omkarkhilari95@gmail.com>
2024-07-15 16:33:46 +0530

    fixes

--- blog-frontend/src/axiosInstance.js
+++ blog-frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
+  baseURL: 'https://omkar.bhaskaraa45.me/',
 });
 
 export default instance;
